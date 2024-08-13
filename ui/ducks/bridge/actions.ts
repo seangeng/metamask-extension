ae60bcc4a7a80b7e655926a26854069292135612
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-08-13 15:36:42 -0700

    feat: update bridge quote params on input change and display quotes

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -1,6 +1,7 @@
 // TODO: Remove restricted import
 // eslint-disable-next-line import/no-restricted-paths
 import { Hex } from '@metamask/utils';
+import { zeroAddress } from 'ethereumjs-util';
 import {
   BridgeBackgroundAction,
   BridgeUserAction,
@@ -11,10 +12,18 @@ import {
 import { forceUpdateMetamaskState } from '../../store/actions';
 import { submitRequestToBackground } from '../../store/background-connection';
 import { MetaMaskReduxDispatch } from '../../store/store';
+import { QuoteRequest } from '../../pages/bridge/types';
+import {
+  AssetWithDisplayData,
+  ERC20Asset,
+  NativeAsset,
+} from '../../components/multichain/asset-picker-amount/asset-picker-modal/types';
+import { Numeric } from '../../../shared/modules/Numeric';
 import { bridgeSlice } from './bridge';
 
 const {
   setToChainId: setToChainId_,
+<<<<<<< HEAD
   setFromToken,
   setToToken,
   setFromTokenInputValue,
@@ -28,9 +37,29 @@ export {
   setFromTokenInputValue,
   switchToAndFromTokens,
   resetInputFields,
+=======
+  setFromToken: setFromToken_,
+  setToToken: setToToken_,
+  setFromTokenInputValue: setFromTokenInputValue_,
+  resetInputFields: resetInputFields_,
+  switchToAndFromTokens,
+} = bridgeSlice.actions;
+
+export { switchToAndFromTokens };
+
+const updateQuoteRequestParams = <T extends Partial<QuoteRequest>>(
+  params: T,
+) => {
+  return async (dispatch: MetaMaskReduxDispatch) => {
+    await submitRequestToBackground(BridgeUserAction.UPDATE_QUOTE_PARAMS, [
+      params,
+    ]);
+    await forceUpdateMetamaskState(dispatch);
+  };
+>>>>>>> 16b712381d (feat: update bridge quote params on input change and display quotes)
 };
 
-const callBridgeControllerMethod = <T>(
+const callBridgeControllerMethod = <T extends string>(
   bridgeAction: BridgeUserAction | BridgeBackgroundAction,
   args?: T[],
 ) => {
@@ -49,7 +78,60 @@ export const setBridgeFeatureFlags = () => {
   };
 };
 
+export const resetBridgeState = () => {
+  return async (dispatch: MetaMaskReduxDispatch) => {
+    dispatch(
+      callBridgeControllerMethod(BridgeBackgroundAction.RESET_STATE, []),
+    );
+  };
+};
+
 // User actions
+export const setFromToken = (
+  payload: AssetWithDisplayData<ERC20Asset> | AssetWithDisplayData<NativeAsset>,
+) => {
+  return async (dispatch: MetaMaskReduxDispatch) => {
+    dispatch(setFromToken_(payload));
+    dispatch(
+      updateQuoteRequestParams<Pick<QuoteRequest, 'srcTokenAddress'>>({
+        srcTokenAddress: payload.address ?? zeroAddress(),
+      }),
+    );
+  };
+};
+
+export const setToToken = (
+  payload: AssetWithDisplayData<ERC20Asset> | AssetWithDisplayData<NativeAsset>,
+) => {
+  return async (dispatch: MetaMaskReduxDispatch) => {
+    dispatch(setToToken_(payload));
+    dispatch(
+      updateQuoteRequestParams<Pick<QuoteRequest, 'destTokenAddress'>>({
+        destTokenAddress: payload.address ?? '',
+      }),
+    );
+  };
+};
+
+export const setFromTokenInputValue = (payload: {
+  amount: QuoteRequest['srcTokenAmount'];
+  decimals: number;
+}) => {
+  return async (dispatch: MetaMaskReduxDispatch) => {
+    dispatch(setFromTokenInputValue_(payload.amount));
+    dispatch(
+      updateQuoteRequestParams<Pick<QuoteRequest, 'srcTokenAmount'>>({
+        srcTokenAmount:
+          payload.amount === ''
+            ? payload.amount
+            : Numeric.from(payload.amount, 10)
+                .shiftedBy(-1 * payload.decimals)
+                .toString(),
+      }),
+    );
+  };
+};
+
 export const setFromChain = (chainId: Hex) => {
   return async (dispatch: MetaMaskReduxDispatch) => {
     dispatch(
