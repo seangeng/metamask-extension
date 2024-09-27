8fb84a49f991b807a45e0a03b5b7fe8fd97f4808
IF <139582705+infiniteflower@users.noreply.github.com>
2024-09-27 16:03:49 -0400

    fix: not using hex for gas field

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -177,7 +177,10 @@ export const signBridgeTransaction = (
       const shouldResetApproval = allowance.lt(sentAmount) && allowance.gt(0);
 
       if (shouldResetApproval) {
-        const gasLimit = quoteMeta.approval.gasLimit.toString();
+        const gasLimit = new Numeric(
+          quoteMeta.approval.gasLimit,
+          10,
+        ).toPrefixedHexString();
         const txParams = getEthUsdtApproveResetTxParams({
           ...quoteMeta.approval,
           chainId: hexChainId,
@@ -222,7 +225,10 @@ export const signBridgeTransaction = (
         await handleEthUsdtAllowanceReset(hexChainId);
       }
 
-      const gasLimit = quoteMeta.approval.gasLimit.toString();
+      const gasLimit = new Numeric(
+        quoteMeta.approval.gasLimit,
+        10,
+      ).toPrefixedHexString();
       const txParams = {
         ...quoteMeta.approval,
         chainId: hexChainId,
@@ -261,7 +267,10 @@ export const signBridgeTransaction = (
         throw new Error('Invalid chain ID');
       }
 
-      const gasLimit = quoteMeta.trade.gasLimit.toString();
+      const gasLimit = new Numeric(
+        quoteMeta.trade.gasLimit,
+        10,
+      ).toPrefixedHexString();
       const txParams = {
         ...quoteMeta.trade,
         chainId: hexChainId,
