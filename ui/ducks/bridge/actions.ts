2dac6571d8189ef50690c091f766c47257b9d1ae
IF <139582705+infiniteflower@users.noreply.github.com>
2024-10-02 15:33:17 -0400

    chore: fix types

--- ui/ducks/bridge/actions.ts
+++ ui/ducks/bridge/actions.ts
@@ -235,8 +235,8 @@ export const signBridgeTransaction = (
       maxFeePerGas,
       maxPriorityFeePerGas,
     }: {
-      maxFeePerGas: string;
-      maxPriorityFeePerGas: string;
+      maxFeePerGas: string | undefined;
+      maxPriorityFeePerGas: string | undefined;
     }) => {
       console.log('Bridge', 'handleApprovalTx');
 
@@ -295,8 +295,8 @@ export const signBridgeTransaction = (
       maxPriorityFeePerGas,
     }: {
       approvalTxId: string | undefined;
-      maxFeePerGas: string;
-      maxPriorityFeePerGas: string;
+      maxFeePerGas: string | undefined;
+      maxPriorityFeePerGas: string | undefined;
     }) => {
       console.log('Bridge', 'handleBridgeTx');
       const hexChainId = new Numeric(
