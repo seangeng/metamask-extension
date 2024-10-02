9f400f68480c469c5cf7474e31a2ce9b9b9f4121
Micaela Estabillo <micaela.estabillo@consensys.net>
2024-10-02 16:52:26 -0700

    chore: setFromChain fix

--- ui/pages/bridge/prepare/prepare-bridge-page.tsx
+++ ui/pages/bridge/prepare/prepare-bridge-page.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import classnames from 'classnames';
 import {
+  setFromChain,
   setFromToken,
   setFromTokenInputValue,
   setToChain,
