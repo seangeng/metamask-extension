2892822f88a1da5e14507e10c097acdcfcd12266
NidhiKJha <menidhikjha@gmail.com>
2024-10-08 11:06:30 +0100

    fixed lint

--- ui/pages/permissions-connect/connect-page/connect-page.tsx
+++ ui/pages/permissions-connect/connect-page/connect-page.tsx
@@ -53,7 +53,6 @@ export const ConnectPage: React.FC<ConnectPageProps> = ({
   permissionsRequestId,
   rejectPermissionsRequest,
   approveConnection,
-  activeTabOrigin,
 }) => {
   const t = useI18nContext();
 
